ea79925782dfc6894c26d9d5a9ef44e203884a70
lazarostar <nascimento.dev26@gmail.com>
2023-01-12 21:34:14 +0100

    feat: get profiles from mongo atlas

---
+++ src/models/Profile.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const ProfileSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  gender: String,
+  dateOfBirth: String,
+  sports: [[String]],
+  location: String,
+  team: String,
+  description: String,
+});
+
+module.exports =
+  mongoose.models.Profile ||
+  mongoose.model("Profile", ProfileSchema, "profile");
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,26 +1,15 @@
 import { z } from "zod";
+import dbConnect from "../../../lib/dbConnect";
+import Profile from "../../../models/Profile";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const profileRouter = createTRPCRouter({
-  all: publicProcedure.input(z.object({ text: z.string() })).query(() => {
-    return [
-      {
-        _id: "1",
-        firstName: "Elton",
-        lastName: "Alcantara",
-        gender: 0,
-        dateOfBirth: "1984-03-02",
-        sports: ["Football", "Baseball"],
-      },
-      {
-        _id: "2",
-        firstName: "Roy",
-        lastName: "Zhang",
-        gender: 1,
-        dateOfBirth: "1999-02-22",
-        sports: ["Soccer", "Archery", "Wrestling"],
-      },
-    ];
+  all: publicProcedure.input(z.object({ text: z.string() })).query(async () => {
+    await dbConnect();
+
+    const profiles = await Profile.find({});
+
+    return profiles;
   }),
 });
